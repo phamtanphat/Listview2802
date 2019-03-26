0be49849c2f4444a35a61b418a65fdbca8f3e3d4
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-03-26 11:15:57 +0700

    lv co ban

--- app/src/main/java/khoapham/ptp/phamtanphat/listview/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/listview/MainActivity.java
@@ -2,17 +2,30 @@ package khoapham.ptp.phamtanphat.listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView lvCoban;
+    ArrayList<String> mangmonhoc = new ArrayList<>();
+    ArrayAdapter banve;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         lvCoban = findViewById(R.id.listview);
-        
+
+        mangmonhoc.add("Toan");
+        mangmonhoc.add("Ly");
+        mangmonhoc.add("Hoa");
+
+        banve = new ArrayAdapter(MainActivity.this,android.R.layout.simple_list_item_1,mangmonhoc);
+        lvCoban.setAdapter(banve);
+
     }
 }
