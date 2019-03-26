923979b9fdec888ee601aed5113f432374e3b9e1
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-03-26 11:48:27 +0700

    lv co ban

--- app/src/main/java/khoapham/ptp/phamtanphat/listview/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/listview/MainActivity.java
@@ -2,6 +2,9 @@ package khoapham.ptp.phamtanphat.listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -19,13 +22,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         lvCoban = findViewById(R.id.listview);
-
         mangmonhoc.add("Toan");
         mangmonhoc.add("Ly");
         mangmonhoc.add("Hoa");
 
         banve = new ArrayAdapter(MainActivity.this,android.R.layout.simple_list_item_1,mangmonhoc);
         lvCoban.setAdapter(banve);
+
+//        lvCoban.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                mangmonhoc.remove(position);
+//                banve.notifyDataSetChanged();
+//            }
+//        });
+        lvCoban.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                mangmonhoc.add(0,"anh van");
+                banve.notifyDataSetChanged();
+                return true;
+            }
+        });
 
     }
 }
